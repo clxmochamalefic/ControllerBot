a3c2dafd696b9aaef3b27deb43182a625e9344e0
cocoalix <cocoalix@gmail.com>
2021-12-12 17:06:11 +0900

    first commit for SerecraftBot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import 'dotenv/config'
 import express, { Request, Response } from 'express';
 import Discord, { Message } from "discord.js";
-import { DISCORD_TOKEN } from './config/secrets';
 import CommandHandler from './commandHandler';
 import config from './config/botConfig';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const client = new Discord.Client();
@@ -29,5 +29,5 @@ client.on("ready", () => { console.log("Hive Greeter has started"); });
 client.on("message", (message: Message) => { commandHandler.handleMessage(message); });
 client.on("error", e => { console.error("Discord client error!", e); });
 
-client.login(DISCORD_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
